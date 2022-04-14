33975dee67190fb0ba9279eb2c044ab914cdd88d
Basixx <barbara_rutkowska.22@wp.pl>
2022-04-14 22:32:41 +0200

    First plane project commit

---
+++ ClothesFactory_Frontend/src/main/java/com/kodilla/clothesfactory_frontend/ClothesFactoryFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.clothesfactory_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesFactoryFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesFactoryFrontendApplication.class, args);
+	}
+
+}
---
+++ ClothesFactory_Frontend/src/test/java/com/kodilla/clothesfactory_frontend/ClothesFactoryFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.clothesfactory_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesFactoryFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
