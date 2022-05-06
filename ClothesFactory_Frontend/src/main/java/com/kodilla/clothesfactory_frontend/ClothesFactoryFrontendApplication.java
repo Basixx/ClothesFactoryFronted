88bac7e16df7e1442ac6a5031f46c15824917d1b
Basixx <barbara_rutkowska.22@wp.pl>
2022-05-06 23:45:10 +0200

    Server fixed

--- ClothesFactory_Frontend/src/main/java/com/kodilla/clothesfactory_frontend/ClothesFactoryFrontendApplication.java
+++ ClothesFactory_Frontend/src/main/java/com/kodilla/clothesfactory_frontend/ClothesFactoryFrontendApplication.java
@@ -3,11 +3,17 @@ package com.kodilla.clothesfactory_frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ClothesFactoryFrontendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClothesFactoryFrontendApplication.class, args);
+//		SpringApplication.run(ClothesFactoryFrontendApplication.class, args);
+		SpringApplication app = new SpringApplication(ClothesFactoryFrontendApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "9000"));
+		app.run(args);
 	}
 
 }
