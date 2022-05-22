0f7b853f1bbeaa6f3b387b112aa5f23eaef5fe6f
Basixx <barbara_rutkowska.22@wp.pl>
2022-05-22 14:03:52 +0200

    Show all users orders for admin added

--- ClothesFactory_Frontend/src/main/java/com/kodilla/clothesfactory_frontend/form/admin/AllUsersForm.java
+++ ClothesFactory_Frontend/src/main/java/com/kodilla/clothesfactory_frontend/form/admin/AllUsersForm.java
@@ -1,6 +1,8 @@
 package com.kodilla.clothesfactory_frontend.form.admin;
 
+import com.kodilla.clothesfactory_frontend.domain.Order;
 import com.kodilla.clothesfactory_frontend.domain.User;
+import com.kodilla.clothesfactory_frontend.service.OrderService;
 import com.kodilla.clothesfactory_frontend.service.UserService;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.grid.Grid;
@@ -9,7 +11,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class AllUsersForm extends VerticalLayout {
 
     private final UserService userService = UserService.getInstance();
+    private final OrderService orderService = OrderService.getInstance();
     private final Grid<User> userGrid = new Grid<>(User.class);
+    private final Grid<Order> orderGrid = new Grid<>(Order.class);
 
     public AllUsersForm() {
         userGrid.setColumns("name", "surname", "phoneNumber", "emailAddress", "password",
@@ -17,5 +21,14 @@ public class AllUsersForm extends VerticalLayout {
         add(new Text("All Users:"));
         add(userGrid);
         userGrid.setItems(userService.getAllUsers());
+
+        userGrid.asSingleSelect().addValueChangeListener(event -> {
+            if(userGrid.asSingleSelect().getValue() == null) {
+                return;
+            }
+            int userId = userGrid.asSingleSelect().getValue().getId().intValue();
+            orderGrid.setItems(orderService.getOrdersByUser(userId));
+            add(orderGrid);
+        });
     }
 }
